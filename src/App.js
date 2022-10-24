58c7627dce909f9cf9a2979e33943e7f2c652317
Christine Lee <sejungrhee@gmail.com>
2022-10-24 14:55:33 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ export default function App() {
       <h1>Korilla React Receipts</h1>
 
       {/* receiptP is prop, pass receipt1,2,3 */}
+      {/* <Receipt receiptP={receipt1} />
+      <Receipt receiptP={receipt2} />
+      <Receipt receiptP={receipt3} /> */}
+      
+      {/* show receipts that are not paid */}
       {receipt1.paid ? null : <Receipt receiptP={receipt1} />}
       {receipt2.paid ? null : <Receipt receiptP={receipt2} />}
       {receipt3.paid ? null : <Receipt receiptP={receipt3} />}
